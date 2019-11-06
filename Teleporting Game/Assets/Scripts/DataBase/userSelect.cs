460e079c9aca68127e63be73ed437f6adc63f259
Darien Caron <caro0103@algonquinlive.com>
2019-11-05 19:33:11 -0500

    Implemented database for level manager script
    Using Xampp as a server host and mySQL as the database, i created a small database that holds level id's and level names. The level manager will get the id from a level if their names are the same. When it changes the level, it (currently) increments the level count and loads a new scene based on level name.

---
+++ Teleporting Game/Assets/Scripts/DataBase/userSelect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class userSelect : MonoBehaviour
+{
+    public string URL;
+    public string[] UserData;
+
+    void Start()
+    {
+
+        StartCoroutine(GetText());
+    }
+
+    // Update is called once per frame
+   IEnumerator GetText()
+    {
+        UnityWebRequest users = UnityWebRequest.Get(URL);
+
+        yield return users.SendWebRequest();
+
+        if(users.isNetworkError || users.isHttpError)
+        {
+            Debug.Log(users.error);
+        }
+        else
+        {
+            string userDataString = users.downloadHandler.text;
+            UserData = userDataString.Split(';');
+
+        }
+
+        Debug.Log(GetDataText(UserData[0], "levelid:"));
+       
+    }
+
+    public string GetDataText(string data, string index)
+    {
+        string value = data.Substring(data.IndexOf(index) + index.Length);
+
+        if(value.Contains("|"))
+        {
+            value = value.Remove(value.IndexOf("|"));
+        }
+        return value;
+    }
+
+   
+   
+}
---
+++ Teleporting Game/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public  class LevelManager : MonoBehaviour
+{
+
+   
+
+    public static LevelManager Instance;
+
+    userSelect databaseSelect;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        databaseSelect = GetComponent<userSelect>();
+
+
+
+        int index = 0;
+        foreach (string s in databaseSelect.UserData)
+        {
+            if (SceneManager.GetActiveScene().name == databaseSelect.GetDataText(databaseSelect.UserData[index], "levelname:"))
+            {
+                int res;
+                int.TryParse(databaseSelect.GetDataText(databaseSelect.UserData[index], "levelid:"), out res);
+                CurrentLevelIndex = res;
+            }
+
+            index++;
+        }
+
+        CurrentLevelIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            ChangeLevels();
+        }
+    }
+
+    void ChangeLevels()
+    {
+        ++CurrentLevelIndex;
+        string sceneToLoad = databaseSelect.GetDataText(databaseSelect.UserData[CurrentLevelIndex], "levelname:");
+        SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+    }
+
+
+    public int CurrentLevelIndex { get; private set; }
+}
